f3366769a941a461a7c61386acb2558bd7006ab1
Samiha Hossain <75538711+Samiha-Hossain19@users.noreply.github.com>
2023-02-12 23:12:37 +0600

    register updated

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [currIndex, setCurrIndex] = useState(0);
+  function plusIndex(n) {
+    setCurrIndex((prev) => prev + n);
+  }
+  function handleNext(e) {
+    e.preventDefault();
+    plusIndex(1);
+  }
   return (
     <div className="register">
       <strong>Sign Up</strong>
@@ -8,9 +16,16 @@ const Register = () => {
       {/*Progress Bar */}
       <div className="progress-bar">
         <ul>
-          <div className="progress"> </div>
+          <div
+            className="progress"
+            style={{ width: `calc(100% / 3 * ${currIndex})` }}
+          />
           {["Name", "Contact", "Birth", "Submit"].map((item, index) => (
-            <li key={item} data-title={item}>
+            <li
+              key={item}
+              data-title={item}
+              className={`${currIndex >= index ? "active" : ""}`}
+            >
               {index + 1}
             </li>
           ))}
@@ -18,9 +33,9 @@ const Register = () => {
       </div>
 
       {/* Pages */}
-      <div className="wrapper">
+      <div className="wrapper" style={{ marginLeft: `${currIndex * -100}%` }}>
         {/* basic info */}
-        <form>
+        <form onSubmit={handleNext}>
           {/*Fname */}
           <label>
             First Name<span>*</span>
@@ -44,8 +59,9 @@ const Register = () => {
             <button type="submit">Next</button>
           </div>
         </form>
+
         {/* Contact Info */}
-        <form>
+        <form onSubmit={handleNext}>
           {/*Email */}
           <label>
             Email<span>*</span>
@@ -67,7 +83,64 @@ const Register = () => {
           {/*Buttons*/}
           <div className="btns">
             <button type="button">Prev</button>
-            <button type="submit">Register</button>
+            <button type="submit">Next</button>
+          </div>
+        </form>
+
+        {/* DOB */}
+        <form onSubmit={handleNext}>
+          {/*Date */}
+          <label>
+            Date<span>*</span>
+          </label>
+          <div className="input-group">
+            <i className="far fa-copy"></i>
+            <input type="date" required />
+          </div>
+
+          {/*Gender */}
+          <label>
+            Gender<span>*</span>
+          </label>
+          <div className="input-group">
+            <select>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </select>
+          </div>
+
+          {/*Buttons*/}
+          <div className="btns">
+            <button type="button">Prev</button>
+            <button type="submit">Next</button>
+          </div>
+        </form>
+
+        {/* Submit */}
+        <form onSubmit={handleNext}>
+          {/*User Name */}
+          <label>
+            Username<span>*</span>
+          </label>
+          <div className="input-group">
+            <i className="fa-solid fa-user"></i>
+            <input type="text" required />
+          </div>
+
+          {/*Password */}
+          <label>
+            Password<span>*</span>
+          </label>
+          <div className="input-group">
+            <i className="fas fa-lock"></i>
+            <input type="password" required />
+          </div>
+
+          {/*Buttons*/}
+          <div className="btns">
+            <button type="button">Prev</button>
+            <button type="submit">Submit</button>
           </div>
         </form>
       </div>
